cd64e6c5c5fad9aa62420de2836d799222c4e189
Matheus Rodrigues <MATHEUSRO@dpc.ad>
2025-08-25 15:39:24 -0300

    Agenda

---
+++ Projetin/fatorial.py
@@ -0,0 +1,12 @@
+# 3. Fatorial Recursivo
+
+# Escreva uma função que calcule o fatorial de um número n usando recursão.
+# Exemplo:
+# n = 5 → 120
+
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * fatorial(n-1)
+
+print(fatorial(5))
